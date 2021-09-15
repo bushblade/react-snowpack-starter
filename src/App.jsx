32cea40f03fae21d351b2a1c4a7b95efc3c8d68c
Will Adams <bushblade@gmail.com>
2021-09-15 10:14:31 +0100

    updates and ignores

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import './App.css'
 function App() {
   // Create the count state.
   const [count, setCount] = useState(0)
+
   // Create the counter (+1 every second).
   useEffect(() => {
     const timer = setTimeout(() => setCount(count + 1), 1000)
     return () => clearTimeout(timer)
   }, [count, setCount])
+
   // Return the App component.
   return (
     <div className='App'>
